29b6b23cfc22ad158d201f29d4002edb9b7f2608
devp <devp@devp-macbookair.roam.corp.google.com>
2015-08-15 12:20:57 -0400

    more

--- lc-js-timer.js
+++ lc-js-timer.js
@@ -1,20 +1,22 @@
 //// LcJsTimerApp
 
-var LcJsTimerApp = function() {
+var LcJsTimerApp = function(eventManager) {
 	console.log("started LcJsTimerApp");
+	this.eventManager_ = eventManager || {};
 	this.gameTimer_ = null;
 	this.travelTimer_ = null;
-};
-
-LcJsTimerApp.instance = null;
-
-LcJsTimerApp.init = function() {
-	return LcJsTimerApp.instance = new LcJsTimerApp();
+	this.setState(LcJsTimerApp.STATES_ENUM.INITIALIZED);
+	this.setTravelState(LcJsTimerApp.STATES_ENUM.TRAVEL_NO);
 };
 
 LcJsTimerApp.prototype.pauseGame = function() {
 	this.pauseGameTimer();
 	this.pauseTravelTimer();
+};
+
+LcJsTimerApp.prototype.resumeGame = function() {
+	this.resumeGameTimer();
+	this.resumeTravelTimer();
 };
 
 LcJsTimerApp.prototype.promptAndConfirmAndStartGameTimer = function() {
@@ -23,7 +25,6 @@ LcJsTimerApp.prototype.promptAndConfirmAndStartGameTimer = function() {
 	var confirmString = "Confirm: start game time of " + timeObj.minutes +
 		":" + timeObj.seconds + "?";
 	if (confirm(confirmString)) {
-		this.stopGameTimer();
 		this.setGameTimer(timeObj.minutes, timeObj.seconds);
 		this.startGameTimer();
 	}
@@ -35,47 +36,103 @@ LcJsTimerApp.prototype.promptAndConfirmAndStartTravelTimer = function() {
 	var confirmString = "Confirm: start travel time of " + timeObj.minutes +
 		":" + timeObj.seconds + "?";
 	if (confirm(confirmString)) {
-		this.stopTravelTimer();
 		this.setTravelTimer(timeObj.minutes, timeObj.seconds);
 		this.startTravelTimer();
 	}
 };
+
+LcJsTimerApp.prototype.confirmStopTravelTimer = function() {
+	var confirmString = "Confirm: stop travel?";
+	if (confirm(confirmString)) {
+		this.stopTravelTimer()
+	};
+}
+
+//// LcJsTimerApp - events
+
+LcJsTimerApp.prototype.onStateChange_ = function(oldState, newState) {
+	if (this.eventManager_.onStateChange) {
+		this.eventManager_.onStateChange(oldState, newState);
+	}
+};
+
+LcJsTimerApp.prototype.onGameTimerTick_ = function(seconds) {
+	if (this.eventManager_.onGameTimerTick) {
+		this.eventManager_.onGameTimerTick(seconds);
+	}
+};
+
+LcJsTimerApp.prototype.onTravelTimerTick_ = function(seconds) {
+	if (this.eventManager_.onTravelTimerTick) {
+		this.eventManager_.onTravelTimerTick(seconds);
+	}
+};
+
+//// LcJsTimerApp - state details
+
+LcJsTimerApp.STATES_ENUM = {};
+// General state, starting at 1
+LcJsTimerApp.STATES_ENUM.INITIALIZED = 1;
+LcJsTimerApp.STATES_ENUM.IN_PROGRESS = 2;
+LcJsTimerApp.STATES_ENUM.PAUSED = 3;
+LcJsTimerApp.STATES_ENUM.STOPPED = 4;
+// Travel states, starting at 100
+LcJsTimerApp.STATES_ENUM.TRAVEL_NO = 100;
+LcJsTimerApp.STATES_ENUM.TRAVEL_YES = 101;
+
+LcJsTimerApp.prototype.setState = function(newState) {
+	var oldState = this.state_ || null;
+	this.state_ = newState;
+	this.onStateChange_(oldState, this.state_);
+};
+
+LcJsTimerApp.prototype.setTravelState = function(newState) {
+	var oldState = this.travelState_ || null;
+	this.travelState_ = newState;
+	this.onStateChange_(oldState, this.state_);
+}
 
 //// LcJsTimerApp - timer details
 
 LcJsTimerApp.prototype.setGameTimer = function(minutes, seconds) {
 	this.gameTimer_ = new LcCountdown(
 		Util.minutesAndSecondsToSeconds(minutes, seconds),
-		this.onGameTimerTick_);
+		this.onGameTimerTick_.bind(this));
 };
 
 LcJsTimerApp.prototype.startGameTimer = function() {
+	this.setState(LcJsTimerApp.STATES_ENUM.IN_PROGRESS);
 	if (this.gameTimer_) {
 		this.gameTimer_.start();
 	}
 };
 
 LcJsTimerApp.prototype.pauseGameTimer = function() {
+	this.setState(LcJsTimerApp.STATES_ENUM.PAUSED);
 	if (this.gameTimer_) {
 		this.gameTimer_.pause();
 	}
 };
 
+LcJsTimerApp.prototype.resumeGameTimer = function() {
+	this.setState(LcJsTimerApp.STATES_ENUM.IN_PROGRESS);
+	if (this.gameTimer_) {
+		this.gameTimer_.resume();
+	}
+};
+
 LcJsTimerApp.prototype.stopGameTimer = function() {
+	this.setState(LcJsTimerApp.STATES_ENUM.STOPPED);
 	if (this.gameTimer_) {
 		this.gameTimer_.stop();
 		this.gameTimer_ = null;
 	}
 };
 
-LcJsTimerApp.prototype.onGameTimerTick_ = function(seconds) {
-	console.log("TICK: GAME: " + seconds);
-};
-
 LcJsTimerApp.prototype.setTravelTimer = function(minutes, seconds) {
 	this.travelTimer_ = new LcCountdown(
 		Util.minutesAndSecondsToSeconds(minutes, seconds),
-		this.onTravelTimerTick_);
+		this.onTravelTimerTick_.bind(this));
 };
 
 LcJsTimerApp.prototype.startTravelTimer = function() {
@@ -90,17 +147,18 @@ LcJsTimerApp.prototype.pauseTravelTimer = function() {
 	}
 };
 
+LcJsTimerApp.prototype.resumeTravelTimer = function() {
+	if (this.travelTimer_) {
+		this.travelTimer_.resume();
+	}
+};
+
 LcJsTimerApp.prototype.stopTravelTimer = function() {
 	if (this.travelTimer_) {
 		this.travelTimer_.stop();
 		this.travelTimer_ = null;
 	}
 };
-
-LcJsTimerApp.prototype.onTravelTimerTick_ = function(seconds) {
-	console.log("TICK: TRAVEL: " + seconds);
-};
-
 
 //// Util
 
